0cf6bf059928d50e066297adad2f719f4e4dc561
Pradeep Prakash <78376385+ipradeepprakash@users.noreply.github.com>
2025-02-28 16:13:07 +0530

    commit changes

---
+++ Open all URLs - Powershell.ps1
@@ -0,0 +1,18 @@
+﻿
+cls
+$url1 = @(
+"https://wordsontech.com/sql-server-always-on-and-read-scale-availability-groups/",
+"https://www.mssqltips.com/sqlservertip/3658/how-to-fix-a-corrupt-msdb-sql-server-database/",
+"https://mssqltrek.com/2020/09/03/sql-server-read-scale-availability-groups-aka-cluster-less-ags/",
+"https://www.sqlshack.com/read-scale-availability-group-in-a-clusterless-availability-group/",
+"https://www.mssqltips.com/sqlservertip/1010/sql-database-snapshots-for-reporting-auditing-and-data-recovery/"
+)
+
+foreach ($a in $url1) {
+
+
+Start-Process -FilePath $a -Wait
+
+
+
+}
